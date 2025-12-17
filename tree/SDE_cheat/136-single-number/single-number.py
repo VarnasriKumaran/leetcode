ab837af43cc2b7a60d14680f88ce8e8f96987f80
Varnasri Kumaran <109358646+VarnasriKumaran@users.noreply.github.com>
2025-12-17 13:54:28 +0530

    Time: 8 ms (22.94%) | Memory: 19.9 MB (13.87%) - LeetSync

---
+++ tree/SDE_cheat/136-single-number/single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count_nums=Counter(nums)
+
+        for i in range(len(nums)):
+            if count_nums[nums[i]]==1:
+                return nums[i]
+        return -1
+
+        
